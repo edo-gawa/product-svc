61a12ac441bf1b5b6cc1522558fbda34d694f3b4
v-ahmadridhwan <v-achmad.ridwan@traveloka.com>
2021-10-30 21:13:23 +0700

    service a

---
+++ src/main/java/com/demo/servicea/controller/Test.java
@@ -0,0 +1,18 @@
+package com.demo.servicea.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    Logger logger = LoggerFactory.getLogger(Test.class);
+
+	
+	@GetMapping("/a")
+		public String test() {
+		logger.info("SRVICE A CALL");
+		    return "hello from A";
+		}
+}
